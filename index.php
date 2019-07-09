5a05d54a1a88191208b47a1e29854b89996040c5
ThePotatoCamera <alex0xela@outlook.com>
2019-07-09 15:41:05 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="common/css/materialize.css"  media="screen,projection"/>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wizard101 - Energy Calculator</title>
+</head>
+<body>
+    <div class="navbar-fixed">
+        <nav class="orange darken-2">
+            <div class="nav-wrapper">
+                <a href="#home" class="brand-logo waves-effect">Energy Calculator</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="https://discord.gg/FQaJFds" class="waves-effect waves-light" target="_blank"><i class="material-icons left">people</i>Join DiscordGamers W101ES Discord Server</a></li>
+                    <li><a href="mailto:incoming+thepotatocamera-energy-calc-13223937-issue-@incoming.gitlab.com?&subject=Bug%20or%20enhancement%20report&body=Remember%20that%20abusing%20this%20system%20will%20cause%20your%20reports%20to%20be%20ignored." target="_top" class="waves-effect waves-light"><i class="material-icons left">bug_report</i>Report issues or enhancements</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    <div class="col s12">
+        <ul class="tabs tabs-fixed-width">
+            <li class="tab"><a class="active waves-effect" href="#home">Home</a></li>
+            <li class="tab"><a class="waves-effect" href="#english">English</a></li>
+            <li class="tab"><a class="waves-effect" href="#spanish">Español</a></li>
+        </ul>
+    </div>
+
+    <div id="home" class="col s12">
+        <div class="container">
+            <h2>Welcome to the energy calculator for Wizard101!</h2>
+            <p>This is a simple calculator that will calculate the time you need to wait before reaching a quantity of energy.<br>
+            To begin, simply select your language and input the needed info.<br>
+            Also, feel free to <a href="https://discord.gg/FQaJFds" target="_blank">join DiscordGamers, a spanish Discord server about Wizard101.</a></p>
+            <br>
+            <h2>¡Bienvenido a la calculadora de energía para Wizard101</h2>
+            <p>Esto es una calculadora simple que te dirá el tiempo que tienes que esperar hasta llegar a cierta cantidad de energía.<br>
+            Para empezas, tan solo selecciona tu idioma y escribe la información que se te pide.<br>
+            No olvides también de <a href="https://discord.gg/FQaJFds" target="_blank">unirte a DiscordGamers, un servidor de Discord en español sobre Wizard101.</a></p>
+        </div>
+    </div>
+    <div id="english" class="col s12">
+    Content
+    </div>
+
+    <div id="spanish" class="col s12">
+    Contenido
+    </div>
+
+    </div>
+    <script type="text/javascript" src="common/js/materialize.js"></script>
+    <script>
+    M.AutoInit();
+    </script>
+</body>
+</html>
